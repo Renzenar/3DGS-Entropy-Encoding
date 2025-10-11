45c24ed64e17a211a8a991a60a2720c370ee4010
Benjamin Szabo <benny03.szabo@gmail.com>
2025-10-11 16:26:03 -0500

    stubbed out project structure

---
+++ target/debug/deps/gaussian_parser-567bb6da620143af.d
@@ -0,0 +1,7 @@
+/home/bens/dev_projects/3DGS-Entropy-Encoding/target/debug/deps/libgaussian_parser-567bb6da620143af.rmeta: crates/gaussian_parser/src/lib.rs
+
+/home/bens/dev_projects/3DGS-Entropy-Encoding/target/debug/deps/libgaussian_parser-567bb6da620143af.rlib: crates/gaussian_parser/src/lib.rs
+
+/home/bens/dev_projects/3DGS-Entropy-Encoding/target/debug/deps/gaussian_parser-567bb6da620143af.d: crates/gaussian_parser/src/lib.rs
+
+crates/gaussian_parser/src/lib.rs:
