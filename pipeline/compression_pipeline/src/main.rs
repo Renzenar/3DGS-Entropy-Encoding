b167213e4cf7c2849bb6d56dcc25331a1e43451a
Benjamin Szabo <benny03.szabo@gmail.com>
2025-10-12 12:57:06 -0500

    perhaps it does work? certainly spits out numbers? :

---
+++ pipeline/compression_pipeline/src/main.rs
@@ -0,0 +1,15 @@
+use gaussian_parser::load_gaussians_from_ply;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path : String = std::env::args().nth(1).expect("Missing .ply file path");
+
+    let scene = load_gaussians_from_ply(&path)?;
+    println!("Loaded {} Gaussians", scene.gaussians.len());
+
+    // Example: inspect the first Gaussian
+    if let Some(g) = scene.gaussians.first() {
+        println!("xyz={:?}, opacity={}, scale={:?}, rot={:?}", g.xyz, g.opacity, g.scale, g.rot);
+        println!("sh_rest_len={}", g.sh_rest.len());
+    }
+    Ok(())
+}
---
+++ target/debug/deps/compression_pipeline-fa1392cff8436fe7.d
@@ -0,0 +1,5 @@
+/home/bens/dev_projects/3DGS-Entropy-Encoding/target/debug/deps/compression_pipeline-fa1392cff8436fe7: pipeline/compression_pipeline/src/main.rs
+
+/home/bens/dev_projects/3DGS-Entropy-Encoding/target/debug/deps/compression_pipeline-fa1392cff8436fe7.d: pipeline/compression_pipeline/src/main.rs
+
+pipeline/compression_pipeline/src/main.rs:
